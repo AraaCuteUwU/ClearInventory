cc8903abe56a7a5f3fac34467774b577842018b8
AraaCuteUwU <71029264+AraaCuteUwU@users.noreply.github.com>
2022-04-15 19:47:22 +0800

    Update Main.php

--- src/AraaCuteUwU/ClearInventory/Main.php
+++ src/AraaCuteUwU/ClearInventory/Main.php
@@ -1,1 +1,36 @@
 
+<?php
+
+namespace AraaCuteUwU\ClearInventory;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\item\ItemFactory;
+use pocketmine\utils\TextFormat as TF;
+
+class Main extends PluginBase {
+	
+	public function onEnable(): void {
+		$this->getLogger()->info("Plugin Enable");
+	}
+	
+	public function onCommand(CommandSender $player, Command $cmd, String $label, Array $args): bool {
+		switch($cmd->getName()){
+			case "clearinventory":
+			if(!$player instanceof Player) {
+				return true;
+			}
+			$count = 0;
+			foreach($player->getInventory()->getContents() as $index => $item){
+				$player->getInventory()->setItem($index, ItemFactory::air());
+				$player->
+				$count++;
+			}
+			$player->sendMessage(TF::GREEN . $count . " item(s) was cleared from your inventory");
+			return true;
+		}
+	}
+}
